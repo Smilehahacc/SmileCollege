efebf117214a71e9c0121aa0dfc7a6891609eaef
Smilehahacc <lin630@vip.qq.com>
2019-03-31 00:45:22 +0800

    test

--- app/src/main/java/com/example/smilecollege/activity/MainActivity.java
+++ app/src/main/java/com/example/smilecollege/activity/MainActivity.java
@@ -1,31 +1,21 @@
 package com.example.smilecollege.activity;
-
-import android.app.Notification;
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-
-import com.example.smilecollege.R;
 import com.example.smilecollege.frament.DynamicFragment;
 import com.example.smilecollege.frament.HomepgaeFragment;
 import com.example.smilecollege.frament.NotificationsFragment;
+import com.example.smilecollege.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -103,7 +93,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_homepage) {
-
+            // Handle the camera action
         } else if (id == R.id.nav_topic) {
 
         }else if (id == R.id.nav_collection) {
