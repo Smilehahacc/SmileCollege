33106b4905389498f57fdaebce089451544c3df8
Smilehahacc <lin630@vip.qq.com>
2019-03-31 23:36:33 +0800

    test

---
+++ app/src/main/java/com/example/smilecollege/activity/FindActivity.java
@@ -0,0 +1,27 @@
+package com.example.smilecollege.activity;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.smilecollege.R;
+
+public class FindActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_layout);
+
+//        设置状态栏颜色
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
+    }
+
+
+}
